a6f7f4d3b7a899778c5d5c7a9453da5548b37240
JoeChindemi <114358613+JoeChindemi@users.noreply.github.com>
2023-10-18 22:36:44 -0400

    Add files via upload

---
+++ build/alphaBuild.js
@@ -0,0 +1,10 @@
+function spinner() {
+    document.getElementsByClassName("loaderOverlay")[0].style.display = "block";
+}
+/*
+function fadeOut() {
+    setTimeout(function () {
+        $("loading").fadeOut('fast');
+    }, 1000);
+}
+*/
